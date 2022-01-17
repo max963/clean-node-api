1bbf8077fc54ad7fc69c20386efd7babc799f6cb
maxwell morais <mg_morais@id.uff.br>
2022-01-17 14:28:01 -0300

    test: ensure return 400 if no email was provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('missing param: name'))
   })
+
+  test('Should return 400 if no email is provider', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_email',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: email'))
+  })
 })
