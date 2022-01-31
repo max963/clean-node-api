1b43dbf4fbdda4888b63b8c00157304842dc3b71
maxwell morais <mg_morais@id.uff.br>
2022-01-31 12:13:23 -0300

    refactor: move all errors imports to a simgle file

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -1,8 +1,6 @@
 import { SignUpController } from './signup-controller'
 import { HttpRequest } from '../protocols/http'
-import { MissingParamError } from '../../errors/missing-param-error'
-import { InvalidParamError } from '../../errors/invalid-param-error'
-import { ServerError } from '../../errors/server-error'
+import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
 import { EmailValidator } from '../protocols/email-validator'
 
 interface SutTypes {
